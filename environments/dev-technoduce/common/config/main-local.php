0bd80a1f55e69b276f0c019871482bf9e138adc1
mariyappan88 <mariyappan@designbox.com.kw>
2016-03-22 16:06:23 +0530

    SMTP settings configured in environment config file....

--- environments/dev-technoduce/common/config/main-local.php
+++ environments/dev-technoduce/common/config/main-local.php
@@ -3,10 +3,11 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=192.168.1.179;dbname=whitebook',
+            'username' => 'svnuser',
+            'password' => 'pXc9nbmrnC',
             'charset' => 'utf8',
+            'tablePrefix'=>'whitebook_',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
@@ -14,7 +15,23 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,  
+             'transport' => [
+            'class' => 'Swift_SmtpTransport',
+            'host' => 'smtp.gmail.com',
+            'username' => 'a.mariyappan88@gmail.com',
+            'password' => 'TN20aq8298',
+            'port' => '465',
+            'encryption' => 'ssl',                        
+            ],          
         ],
+    ],
+    'modules' => [
+       'admin' => [
+           'class' => 'backend\modules\admin\Module',
+       ],
+        'vendor' => [
+           'class' => 'backend\modules\vendor\Module',
+       ],
     ],
 ];
