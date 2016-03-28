389721858b0abcb88188d0655e8d36dc6f88fd76
Khalid Al-Mutawa <contact@bawes.net>
2016-03-28 12:16:15 +0300

    Update composer dependencies and mail config for production env

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -11,6 +11,19 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.sendgrid.net',
+                'username' => 'sendgridusername',
+                'password' => 'sgridpassword',
+                'port' => '587',
+                'encryption' => 'tls',
+                'plugins' => [
+                    [
+                        'class' => 'Openbuildings\Swiftmailer\CssInlinerPlugin',
+                    ],
+                ],
+            ],
         ],
     ],
 ];
