0792d82df8b3caf79d30543f3ca4eb50cba11fac
Khalid Al-Mutawa <khalid@bawes.net>
2016-03-28 13:27:33 +0300

    Adjust backend entryscript - included vendor and admin portal links

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -4,11 +4,13 @@ namespace backend\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
+use yii\helpers\Url;
+use yii\helpers\Html;
 /**
  * Site controller
  */
 class SiteController extends Controller
-{		
+{
     /**
      * @inheritdoc
      */
@@ -19,5 +21,15 @@ class SiteController extends Controller
                 'class' => 'yii\web\ErrorAction',
             ],
         ];
+    }
+
+    public function actionIndex()
+    {
+        $vendorLink = Url::to("vendor/default/login");
+        $adminLink = Url::to("admin/site/login");
+
+        echo Html::a("Link to Vendor Backend", $vendorLink); //vendor/default/login
+        echo "<br/>";
+        echo Html::a("Link to Admin Backend", $adminLink); //vendor/default/login
     }
 }
