51bfc36140f7a90ec92d776d3270ecb57492f7d7
mariyappan88 <mariyappan@designbox.com.kw>
2016-03-30 10:51:41 +0530

    Declared aliases in bootstrap file and removed extra square bracket in main file.

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,15 +4,15 @@ Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 
+Yii::setAlias('frontend_app_images', '/frontend/web/images'); // frontend
+Yii::setAlias('uploads', '/backend/web/uploads/'); // backend
 
-Yii::setAlias('@frontend_app_images', realpath(dirname(__FILE__).'/frontend/web/images/'));
-Yii::setAlias('@vendor_images', realpath(dirname(__FILE__).'/backend/web/uploads/vendor_images/'));
-Yii::setAlias('@sales_guide_images', realpath(dirname(__FILE__).'@app/web/uploads/guide_images/'));
-Yii::setAlias('@vendor_item_images_210', realpath(dirname(__FILE__).'/backend/web/uploads/vendor_images/'));
-Yii::setAlias('@sub_category', realpath(dirname(__FILE__).'@app/web/uploads/subcategory_icon/'));
-Yii::setAlias('@vendor_image', realpath(dirname(__FILE__).'/backend/web/uploads/vendor_images/'));
-Yii::setAlias('@gif_img', realpath(dirname(__FILE__).'frontend/web/images/ajax-loader.gif'));
-Yii::setAlias('@sub_category', realpath(dirname(__FILE__).'/web/uploads/subcategory_icon/'));
-Yii::setAlias('@top_category', realpath(dirname(__FILE__).'/web/uploads/category_ads/top/'));
-Yii::setAlias('@bottom_category', realpath(dirname(__FILE__).'/web/uploads/category_ads/bottom/'));
-Yii::setAlias('@home_ads', realpath(dirname(__FILE__).'/web/uploads/home_ads/')); 
+Yii::setAlias('vendor_images', '@uploads/vendor_images/');
+Yii::setAlias('@sales_guide_images', '@uploads/guide_images/');
+Yii::setAlias('@vendor_item_images_210', '@uploads/vendor_images/');
+Yii::setAlias('@sub_category', '@uploads/subcategory_icon/');
+Yii::setAlias('@gif_img', '@frontend_app_images/ajax-loader.gif');
+Yii::setAlias('@sub_category', '@uploads/subcategory_icon/');
+Yii::setAlias('@top_category', '@uploads/category_ads/top/');
+Yii::setAlias('@bottom_category', '@uploads/category_ads/bottom/');
+Yii::setAlias('@home_ads', '@uploads/home_ads/'); 
