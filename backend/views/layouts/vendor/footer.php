6191e5e3ad9777114ff21f27a1d1dfc5fcfb3a6b
Khalid Al-Mutawa <khalid@bawes.net>
2016-04-01 14:40:39 +0000

    Refactor core js from footer into AppAsset

--- backend/views/layouts/vendor/footer.php
+++ backend/views/layouts/vendor/footer.php
@@ -1,7 +1,4 @@
 </div>
 <!-- END CONTENT --> 
-<!-- BEGIN PAGE LEVEL SCRIPTS -->
-<script src="<?php echo Yii::$app->themeURL->createAbsoluteUrl(''); ?>js/core.js" type="text/javascript"></script>
-
 </body>
 </html>
