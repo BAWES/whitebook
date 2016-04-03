df7c42d869d90f465285b8e0ec1fc00759c339cd
Khalid <khalid@bawes.net>
2016-04-03 15:35:14 +0300

    Refactor admin module into admin application

--- admin/views/site/error.php
+++ admin/views/site/error.php
@@ -2,21 +2,27 @@
 
 use yii\helpers\Html;
 
+
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
 /* @var $exception Exception */
-/************* commmon and frontend ***************************/
+
 $this->title = $name;
 ?>
 <div class="site-error">
-    <h1>Not Found (#404)</h1>
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
     <div class="alert alert-danger">
-        Page not found.    </div>
+        <?= nl2br(Html::encode($message)) ?>
+    </div>
+
     <p>
         The above error occurred while the Web server was processing your request.
     </p>
     <p>
-      123456  Please contact us if you think this is a server error. Thank you.
+        contact us if you think this is a server error. Thank you.
     </p>
+
 </div>
