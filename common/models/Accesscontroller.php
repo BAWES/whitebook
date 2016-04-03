cfce81356416aa8073a7321e6364cc78ca9897e8
Khalid <khalid@bawes.net>
2016-04-03 15:17:44 +0300

    Refactor all common models into common directory

---
+++ common/models/Accesscontroller.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace common\models;
+
+use common\models\Usercontroller;
+use Yii;
+
+/**
+* This is the model class for table "{{%access_control}}".
+*
+* @property integer $access_id
+* @property string $role_id
+* @property string $admin_id
+* @property integer $created_by
+* @property integer $modified_by
+* @property string $created_datetime
+* @property string $modified_datetime
+*
+* @property Admin $admin
+* @property Role $role
+*/
+class Accesscontroller extends \yii\db\ActiveRecord
+{
+    /**
+    * @inheritdoc
+    */
+    public static function tableName()
+    {
+        return '{{%access_control}}';
+    }
+
+    /**
+    * @inheritdoc
+    */
+    public function rules()
+    {
+        return [
+            [['admin_id','controller','create','update','delete','manage','view',], 'required'],
+            //[['role_id', 'admin_id'], 'integer'],
+            [['created_datetime', 'modified_datetime'], 'safe'],
+        ];
+    }
+
+    /**
+    * @inheritdoc
+    */
+    public function attributeLabels()
+    {
+        return [
+            'access_id' => 'Access User',
+            'role_id' => 'Roles',
+            'controller' => 'Controller',
+            'auth_item' => 'Functions',
+            'admin_id' => 'User',
+            'created_by' => 'Created By',
+            'modified_by' => 'Modified By',
+            'created_datetime' => 'Created Datetime',
+            'modified_datetime' => 'Modified Datetime',
+        ];
+    }
+
+    /**
+    * @return \yii\db\ActiveQuery
+    */
+    public static function getAdmin()
+    {
+        return $this->hasOne(Admin::className(), ['id' => 'admin_id']);
+    }
+
+
+
+    public static function getAdminName($id)
+    {
+        $admin= Admin::find()
+        ->select ('admin_name')
+        ->where(['=', 'id', $id])
+        ->one();
+        return ($admin['admin_name']);
+    }
+    public static function getControllerName($id)
+    {
+        $controller= Usercontroller::find()
+        ->select ('controller')
+        ->where(['=', 'id', $id])
+        ->one();
+        return ($controller['controller']);
+    }
+
+    public static function itemcontroller($ctrllist)
+    {
+        $k=explode(",",$ctrllist);
+
+        $g=array();
+        foreach ($k as $f)
+        {
+            $controller= Usercontroller::find()
+                ->select ('controller')
+                ->where(['=', 'id', $f])
+                ->one();
+            $g[]=$controller;
+        }
+        $m=array();
+        foreach ($g as $r)
+        {
+            $m[]= $r['controller'];
+        }
+        return implode(" , ",$m);
+    }
+
+    /**
+    * @return \yii\db\ActiveQuery
+    */
+    public function getRole()
+    {
+        return $this->hasOne(Role::className(), ['role_id' => 'role_id']);
+    }
+}
---
+++ common/models/AdminSearch.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * AdminSearch represents the model behind the search form about `common\models\Admin`.
+ */
+class AdminSearch extends Admin
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'role_id'], 'integer'],
+            [['admin_name', 'admin_email', 'admin_password', 'admin_status'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Admin::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+			'sort'=> ['defaultOrder' => ['id'=>SORT_DESC]]
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'role_id' => $this->role_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'admin_name', $this->admin_name])
+            ->andFilterWhere(['like', 'admin_email', $this->admin_email])
+            ->andFilterWhere(['like', 'admin_password', $this->admin_password])
+            ->andFilterWhere(['like', 'admin_status', $this->admin_status]);
+
+        return $dataProvider;
+    }
+}
