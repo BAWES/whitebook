113bde17072785eee46b8e720e0d5557f1a30771
Khalid <khalid@bawes.net>
2016-04-03 13:43:11 +0300

    Update params, comment out some configs causing errors

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'appImageUrl'=> $_SERVER['HTTPS'].'/backend/web/uploads/app_img/',
-    'vendorImageUrl'=> $_SERVER['HTTPS'].'/backend/web/uploads/vendor_images/',
+    'adminEmail' => 'khalid@bawes.net',
+    //'appImageUrl'=> $_SERVER['HTTPS'].'/backend/web/uploads/app_img/',
+    //'vendorImageUrl'=> $_SERVER['HTTPS'].'/backend/web/uploads/vendor_images/',
     'dateFormat'=> 'php:d/m/Y',
     'Currency' => 'KD',
 ];
