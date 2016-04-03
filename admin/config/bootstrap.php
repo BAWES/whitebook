6747b2c548c8c4f86c82d5705da5332ac4500a5c
Khalid <khalid@bawes.net>
2016-04-03 16:03:17 +0300

    Fix bootstrap configurations

--- admin/config/bootstrap.php
+++ admin/config/bootstrap.php
@@ -1,2 +1,13 @@
 <?php
 
+Yii::setAlias('uploads', '/admin/web/uploads/'); // backend
+
+Yii::setAlias('vendor_images', '@uploads/vendor_images/');
+Yii::setAlias('sales_guide_images', '@uploads/guide_images/');
+Yii::setAlias('vendor_item_images_210', '@uploads/vendor_images/');
+Yii::setAlias('sub_category', '@uploads/subcategory_icon/');
+Yii::setAlias('gif_img', '@frontend_app_images/ajax-loader.gif');
+Yii::setAlias('sub_category', '@uploads/subcategory_icon/');
+Yii::setAlias('top_category', '@uploads/category_ads/top/');
+Yii::setAlias('bottom_category', '@uploads/category_ads/bottom/');
+Yii::setAlias('home_ads', '@uploads/home_ads/');
