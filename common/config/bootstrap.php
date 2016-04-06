00f5b00db7b99154a95accc80245b264bcc15328
Khalid <khalid@bawes.net>
2016-04-06 17:32:48 +0300

    Setup initial S3 storage configurations

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -7,3 +7,9 @@ Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('themes', dirname(dirname(dirname(dirname(dirname($_SERVER["REQUEST_URI"]))))) . '/admin/web/themes/default/'); // backend
 
 Yii::setAlias('frontend_app_images', '/frontend/web/images'); // frontend
+
+//Amazon S3 Alias
+Yii::setAlias('s3','https://whitebook-files.s3.amazonaws.com');
+Yii::setAlias('salesguides','@s3/sales_guide_images');
+Yii::setAlias('vendorimages','@s3/vendor_images');
+Yii::setAlias('vendoritem','@s3/vendor_item_images_210');
--- common/config/main.php
+++ common/config/main.php
@@ -4,6 +4,17 @@ return [
     'name' => 'The White Book',
     'vendorPath' => dirname(dirname(__DIR__)).'/vendor',
     'components' => [
+        'resourceManager' => [
+            'class' => 'common\components\S3ResourceManager',
+            'key' => 'AKIAJJCSTGJVP4LXDOJQ',
+            'secret' => '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',
+            'bucket' => 'whitebook-files'
+            /**
+             * You can access the bucket with:
+             * https://whitebook-files.s3.amazonaws.com/
+             * https://whitebook-files.s3.amazonaws.com/folderName/fileName.jpg
+             */
+        ],
         'slack' => [
             'class' => 'understeam\slack\Client',
             'url' => 'https://hooks.slack.com/services/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',
