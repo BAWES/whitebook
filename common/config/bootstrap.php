2b8de8b6d21ea111aa1aa0a8d45df07a39b3bd77
Khalid <khalid@bawes.net>
2016-04-08 00:26:36 +0300

    Disabled theme alias as its config breaks console commands

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,8 +4,8 @@ Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('admin', dirname(dirname(__DIR__)) . '/admin');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
-Yii::setAlias('themes', dirname(dirname(dirname(dirname(dirname($_SERVER["REQUEST_URI"]))))) . '/admin/web/themes/default/'); // backend
 
+Yii::setAlias('themes', '/admin/web/themes/default/'); // backend
 Yii::setAlias('frontend_app_images', '/frontend/web/images'); // frontend
 
 //Amazon S3 Alias
