eff8295e88f2de365fd3615b34321f5645c2f52c
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-11 13:34:24 +0530

    Aliases changed due to path rearranged...

--- frontend/config/bootstrap.php
+++ frontend/config/bootstrap.php
@@ -1,13 +1,11 @@
 <?php
-Yii::setAlias('uploads', '/backend/web/uploads'); // backend
-Yii::setAlias('directory', dirname(dirname(dirname(dirname(dirname($_SERVER["REQUEST_URI"]))))).Yii::getAlias('@uploads')); 
-
-Yii::setAlias('vendor_images', '@directory/vendor_images');
-Yii::setAlias('sales_guide_images', '@directory/guide_images');
-Yii::setAlias('vendor_item_images_210', '@directory/vendor_images'); // product images
+Yii::setAlias('uploads', '/admin/web/uploads'); // backend
+Yii::setAlias('vendor_images', '@uploads/vendor_images');
+Yii::setAlias('sales_guide_images', '@uploads/guide_images');
+Yii::setAlias('vendor_item_images_210', '@uploads/vendor_images'); // product images
 Yii::setAlias('sub_category', '@uploads/subcategory_icon');
 Yii::setAlias('gif_img', '@frontend_app_images/ajax-loader.gif');
-Yii::setAlias('sub_category', '@directory/subcategory_icon');
-Yii::setAlias('top_category', '@directory/category_ads/top');
-Yii::setAlias('bottom_category', '@directory/category_ads/bottom');
-Yii::setAlias('home_ads', '@directory/home_ads');
+Yii::setAlias('sub_category', '@uploads/subcategory_icon');
+Yii::setAlias('top_category', '@uploads/category_ads/top');
+Yii::setAlias('bottom_category', '@uploads/category_ads/bottom');
+Yii::setAlias('home_ads', '@uploads/home_ads');
