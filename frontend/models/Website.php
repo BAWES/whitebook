137dc47bec609004a5718ecd79d9bad2f0353983
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-13 15:17:56 +0530

    Changed SQL to AR on frontend website.php model

--- frontend/models/Website.php
+++ frontend/models/Website.php
@@ -3,6 +3,12 @@
 namespace frontend\models;
 
 use common\models\User;
+use common\models\Siteinfo;
+use common\models\Socialinfo;
+use common\models\Category;
+use common\models\Adverthome;
+use common\models\Slide;
+use common\models\Eventtype;
 use yii\base\Model;
 use Yii;
 use yii\db\Query;
@@ -10,53 +16,24 @@ use yii\db\Query;
 class Website extends Model {
 
     public static function get_general_settings() {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT * FROM whitebook_siteinfo WHERE id=1');
-        $general = $command->queryAll();
-
-        return $general;
+        return $general = Siteinfo::find()->where(['id'=>1])->asArray()->all();
     }
 
     public static function get_social_network() {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT * FROM whitebook_social_info WHERE store_social_id=1');
-        $social = $command->queryAll();
-
-        return $social;
-    }
-
-    public static function get_email_template() {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT * FROM whitebook_email_template');
-        $email_template = $command->queryAll();
-
-        return $email_template;
+         return $social = Socialinfo::find()->where(['store_social_id'=>1])->asArray()->all();
     }
 
     public static function get_main_category() {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT category_id,category_icon,category_name,category_url FROM whitebook_category WHERE parent_category_id IS NULL and trash="Default" and category_allow_sale="yes"');
-        $general = $command->queryAll();
-
-        return $general;
+        return $general = Category::find()->select('category_id,category_icon,category_name,category_url')->where(['parent_category_id'=>'IS NULL', 'trash'=>"Default",'category_allow_sale'=>"yes"])->asArray()->all();
     }
 
-
     public static function get_home_ads() {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT advert_code FROM whitebook_advert_home where trash="Default" limit 1');
-        $ads = $command->queryAll();
-
-        return $ads;
+        return $ads = Adverthome::find()->select('advert_code')->where(['trash'=>'Default'])->limit(1)->asArray()->one();
     }
 
     public static function get_banner_details() {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT * FROM whitebook_slide where trash="Default" and slide_status="Active" order by sort');
-        $ads = $command->queryAll();
-
-        return $ads;
-    }
+        return $ads = Slide::find()->select('*')->where(['trash'=>'Default','slide_status'=>"Active"])->asArray()->all();
+    }    
 
     public static function get_search_directory_list($categoryid) {
         $today = date('Y-m-d H:i:s');
@@ -103,72 +80,55 @@ class Website extends Model {
                 ->LIMIT(50);
         $command = $query->createCommand();
         $data = $command->queryAll();
-
         return $data;
     }
 
     public static function get_category_id($category_url) {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT category_id FROM whitebook_category WHERE category_url="' . $category_url . '"');
-        $general = $command->queryAll();
-
+        $general = Category::find()->select('category_id')->where(['category_url'=>$category_url])->asArray()->all();
         return $general[0]['category_id'];
     }
 
     public static function vendor_details($slug) {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT * FROM whitebook_vendor WHERE slug="' . $slug . '"');
-        $vendor = $command->queryAll();
-
-        return $vendor;
+        return $vendor = Vendor::find()->select('*')->where(['slug'=>$slug])->asArray()->all();
     }
 
     public static function vendor_item_details($id) {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT * FROM whitebook_vendor_item WHERE item_for_sale="yes" and item_approved="yes" and item_status="active" and vendor_id="' . $id . '"');
-        $vendor = $command->queryAll();
-
-        return $vendor;
+        return $vendor = Vendoritem::find()->select('*')->where(['item_for_sale'=>'yes','item_approved'=>"yes",
+        'item_status'=>"active", 'vendor_id'=>$id])->asArray()->all();
     }
 
     public static function vendor_social_info($vendor_id) {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT * FROM whitebook_vendor_social_info WHERE vendor_id="' . $vendor_id . '"');
-        $vendor = $command->queryAll();
-
-        return $vendor;
+        return $vendor = (new Query())
+                ->select('*')
+                ->from('whitebook_vendor_social_info')
+                ->where(['vendor_id' => $vendor_id])
+                ->all();
     }
 
     public static function getSEOdata($table = '', $field = '', $value = '', $data = '') {
-        $command = Yii::$app->DB->createCommand("SELECT $data FROM {{%$table}} WHERE $field=$value");
-        $result = $command->queryAll();
-
-        return $result;
+        return $result = (new Query())
+            ->select($data)
+            ->from("{{%$table}}")
+            ->where([$field=>$value])
+            ->all();
     }
 
     public static function get_event_types() {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT type_name,type_id FROM whitebook_event_type');
-        $event_type = $command->queryAll();
-
-        return $event_type;
+        return $event_type = Eventtype::find()->select('type_name,type_id')->asArray()->all();
     }
 
     public static function get_customer_events($customer_id) {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT event_id,event_name,event_type,event_date,slug FROM whitebook_events WHERE customer_id="' . $customer_id . '" order by event_date asc');
-        $events = $command->queryAll();
-
-        return $events;
+     return $events = Events::find()->select('event_id,event_name,event_type,event_date,slug')->where(['customer_id'=>$customer_id])->OrderBy('event_date ASC')->asArray()->all();
     }
 
     public static function check_user_fav($item_id) {
         $customer_id = CUSTOMER_ID;
-        $command = Yii::$app->DB->createCommand(
-                'SELECT wish_status FROM whitebook_wishlist WHERE item_id="' . $item_id . '" and customer_id="' . $customer_id . '"');
-        $user_fav = $command->queryAll();
-
-        return $user_fav;
+        return $user_fav = (new Query())
+            ->select($data)
+            ->from('{{%_wishlist}}')
+            ->where(['item_id'=>$item_id])
+            ->andwhere(['customer_id'=>$customer_id])
+            ->all();
     }
 
     public static function get_products_list($limit, $offset) {
@@ -196,23 +156,23 @@ class Website extends Model {
     }
 
     public static function get_user_event_types($customer_id) {
-        $command = Yii::$app->DB->createCommand("SELECT DISTINCT event_name,event_id
-                  FROM whitebook_events
-                  INNER JOIN whitebook_event_type
-                  ON whitebook_events.event_type=whitebook_event_type.type_name
-                  WHERE whitebook_event_type.trash='default' and whitebook_events.customer_id='$customer_id'");
-        $event_type1 = $command->queryAll();
-
-        return $event_type1;
+        $query = new Query;
+        $query  ->select([
+                'whitebook_events.event_name', 
+                'whitebook_events.event_id'])  
+            ->from('whitebook_events')
+            ->join('INNER JOIN', 'whitebook_event_type',
+                        'whitebook_events.event_type =whitebook_event_type.type_name')
+            ->andwhere('whitebook_vendor_item.trash ="default"')
+            ->andwhere('whitebook_events.customer_id ='.$customer_id)
+            ->orderBy(['item_id' => SORT_DESC]);
+        $command = $query->createCommand();
+        return $event_type1 = $command->queryAll();
     }
 
     // user event type birthday, wedding,etc
     public static function get_user_event($customer_id) {
-        $command = Yii::$app->DB->createCommand(
-                'SELECT DISTINCT event_type FROM whitebook_events where customer_id =' . $customer_id);
-        $event_type = $command->queryAll();
-
-        return $event_type;
+        return $event_type = Events::find()->select('event_type')->where(['customer_id'=>$customer_id])->asArray()->distinct()-> all();
     }
 
     // GET SEO DATA
