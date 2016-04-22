3ee05dd2030be8d56c6171d5ffb441795e488dc9
whitebook-siva <Siva@designbox.com.kw>
2016-04-22 12:25:04 +0530

    CIty controller convert raw SQL into AR

--- admin/controllers/CityController.php
+++ admin/controllers/CityController.php
@@ -209,8 +209,7 @@ class CityController extends Controller
             $data = Yii::$app->request->post();
         }
         $status = ($data['status'] == 'Active' ? 'Deactive' : 'Active');
-        $command = \Yii::$app->db->createCommand('UPDATE whitebook_city SET status="'.$status.'" WHERE city_id='.$data['cid']);
-        $command->execute();
+        $command=Vendor::updateAll(['status' => $status],'city_id= '.$data['cid']);
         if ($status == 'Active') {
             return \yii\helpers\Url::to('@web/uploads/app_img/active.png');
         } else {
