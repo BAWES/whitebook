9eefbc890bd613c9d6402ea4d15d8fbd82a68099
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-27 16:46:37 +0530

    Added LocationSearch model file in admin folder

---
+++ admin/models/LocationSearch.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace admin\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Location;
+
+/**
+ * LocationSearch represents the model behind the search form about `common\models\Location`.
+ */
+class LocationSearch extends Location
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'country_id', 'city_id'], 'integer'],
+            [['location'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Location::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+			'sort'=> ['defaultOrder' => ['id'=>SORT_DESC]]
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'country_id' => $this->country_id,
+            'city_id' => $this->city_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'location', $this->location]);
+
+        return $dataProvider;
+    }
+}
