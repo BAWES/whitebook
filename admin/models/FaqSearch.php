2d58010f00a48f18de3b7ea0a0c6332274f8bf87
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-27 12:40:29 +0530

    FAQ model files added in admin model folder

---
+++ admin/models/FaqSearch.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace admin\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use admin\models\Faq;
+
+/**
+ * FaqSearch represents the model behind the search form about `common\models\Faq`.
+ */
+class FaqSearch extends Faq
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['faq_id', 'created_by', 'modified_by'], 'integer'],
+            [['question', 'answer', 'faq_status', 'created_datetime', 'modified_datetime', 'trash'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+		$query = Faq::find()
+        ->where(['!=', 'trash', 'Deleted'])
+		->orderBy('faq_id');
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+			'sort'=> ['defaultOrder' => ['faq_id'=>SORT_DESC]]
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+        ]);
+
+        $query->andFilterWhere(['like', 'question', $this->question])
+            ->andFilterWhere(['like', 'answer', $this->answer]);
+        return $dataProvider;
+    }
+}
