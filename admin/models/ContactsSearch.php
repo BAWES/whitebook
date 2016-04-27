2d4f6c1f47d2fe226df5672e56c0784c2e1bc959
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-27 11:50:06 +0530

    Changed namesapce in contacts models

--- admin/models/ContactsSearch.php
+++ admin/models/ContactsSearch.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace common\models;
+namespace admin\models;
 
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use common\models\Contacts;
+use admin\models\Contacts;
 
 /**
  * ContactsSearch represents the model behind the search form about `common\models\Contacts`.
