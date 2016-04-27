e31cf719420b0282f745c64d0f5a265ef1bff6a9
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-27 18:41:45 +0530

    Category functions splited and now updated required functions.

--- common/models/Category.php
+++ common/models/Category.php
@@ -36,18 +36,19 @@ class Category extends \yii\db\ActiveRecord
     public $category_icon;
     public static function tableName()
     {
-    	return '{{%category}}';        
+     return '{{%category}}';        
     }
 
-	public function behaviors()
-	{
-	      return [
-	          [
-	              'class' => SluggableBehavior::className(),
-	              'attribute' => 'category_name',	             
-	          ],
-	      ];
-	}
+    public function behaviors()
+    {
+          return [
+              [
+                  'class' => SluggableBehavior::className(),
+                  'attribute' => 'category_name',              
+              ],
+          ];
+    }
+
 
     /**
      * @inheritdoc
@@ -55,7 +56,7 @@ class Category extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-			['category_name','categoryvalidation','on' => 'insert',],			
+			         ['category_name','categoryvalidation','on' => 'insert',],			
             [['parent_category_id', 'created_by', 'modified_by',], 'integer'],
             [['trash', 'category_meta_title', 'category_meta_keywords', 'category_meta_description'], 'string'],
             [['category_name', 'category_meta_title', 'category_meta_keywords', 'category_meta_description'], 'required'],
@@ -69,7 +70,7 @@ class Category extends \yii\db\ActiveRecord
     
     public function scenarios()
     {
-		$scenarios = parent::scenarios();      
+		      $scenarios = parent::scenarios();      
         $scenarios['sub_update'] = ['parent_category_id','category_name',];//Scenario Values Only Accepted
         return $scenarios;
     }
@@ -93,14 +94,6 @@ class Category extends \yii\db\ActiveRecord
             'category_meta_keywords' => 'Category meta keywords',
             'category_meta_description' => 'Category meta description',
         ];
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getAdvertCategories()
-    {
-        return $this->hasMany(AdvertCategory::className(), ['category_id' => 'category_id']);
     }
 
     /**
@@ -136,224 +129,16 @@ class Category extends \yii\db\ActiveRecord
     }
     
     public static function statusImageurl($sale)
-	{			
-		if($sale == 'yes')		
-		return \yii\helpers\Url::to('@web/uploads/app_img/active.png');
-		return \yii\helpers\Url::to('@web/uploads/app_img/inactive.png');
-	}
-	
-	public static function statusTitle($sale)
-	{			
-		if($sale == 'yes')		
-		return 'Active';
-		return 'Deactive';
-	}
-	
-	public  function categoryvalidation($attribute_name,$params)
-	{
-		if(!empty($this->category_name) ){
-		$model = Category::find()
-		->where(['category_name'=>$this->category_name])
-		->andwhere(['parent_category_id'=>null])->one();
-        if($model){
-        $this->addError('category_name','Please enter a unique category name');
-        }
-		}
-	}
-	
-	public static function loadcategoryname()
-	{
-			$category= Category::find()
-			->where(['category_allow_sale'=>'yes'])
-			->andwhere(['!=', 'trash', 'Deleted'])
-			->andwhere(['=', 'category_level', '0'])
-			->andwhere(['parent_category_id' => null])
-			->all();
-			$category=ArrayHelper::map($category,'category_id','category_name');
-			return $category;
-	}
-
-	/* BEGIN LOAD CATEGORY FOR EVENT DETAIL PAGE*/
-	public static function loadcategoryevents()
-	{
-			$category= Category::find()
-			->where(['category_allow_sale'=>'yes'])
-			->andwhere(['!=', 'trash', 'Deleted'])
-			->andwhere(['=', 'category_level', '0'])
-			->andwhere(['parent_category_id' => null])
-			->all();			
-			return $category;
-	}
-	/* END LOAD CATEGORY FOR EVENT DETAIL PAGE*/	
-	
-	
-	public static function loadcategory()
-	{
-		$categories=Category::find()
-		->where(['category_allow_sale' => 'yes'])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->andwhere(['parent_category_id' => null])
-		->all();
-		$category=ArrayHelper::map($categories,'category_id','category_name');
-		return $category;
-	}
-
-	
-		public static function loadgrandcategory()
-	{
-		$categories=Category::find()
-		->where(['category_allow_sale' => 'yes'])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->andwhere(['=', 'category_level', '0'])
-		->andwhere(['parent_category_id' => null])
-		->all();
-		$category=ArrayHelper::map($categories,'category_id','category_name');
-		return $category;
-	}
-	
-	public static function vendorcategoryname($id)
-	{
-		$categories=Category::find()
-		->where(['category_allow_sale' => 'yes'])
-		->andwhere(['vendor_id' => $id])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->all();
-		$category=ArrayHelper::map($categories,'category_id','category_name');
-		return $category;
-	}
-	
-		public static function categorylevel($id)
-	{
-		
-		$categories=Category::find()
-		->select(['category_level'])
-		->where(['category_allow_sale' => 'yes'])
-		->andwhere(['=', 'category_id', $id])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->one();
-		return  $categories['category_level']; 
-	}
-	
-	// for advert category joi two array
-	public static function loadcategoryset($cat)
-	{
-		$categ=array();
-		if(count($cat)>2){
-		foreach($cat as $c)
-		{
-		$categories=Category::find()
-		->select(['category_id','category_name'])
-		->where(['category_allow_sale' => 'yes'])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->andwhere(['=', 'category_id', $c])
-		->all();
-		
-		 $categ[]=ArrayHelper::map($categories,'category_id','category_name');
-		} 
-		return ($categ); 
-	}
-		else
-	{   
-		$categories=Category::find()
-		->select(['category_id','category_name'])
-		->where(['category_allow_sale' => 'yes'])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->andwhere(['=', 'category_id', $cat[0]])
-		->all();
-		  $cat[]=ArrayHelper::map($categories,'category_id','category_name'); 
-	} 
-	}
-
-    public static function vendorcategory($id)
-    {      
-         $vendor = Vendor::find()->select(['category_id'])->where(['vendor_id' => $id])->all();
-         $vendor_id = $vendor[0]['category_id'];         
-         $vendor_exp = explode(',',$vendor_id);
-         $vendor_imp = implode('","',$vendor_exp);
-		$categories = Category::find()
-		->select(['category_id','category_name'])
-		->where(['IN', 'category_id', $vendor_imp])
-		->all();
-         $category =ArrayHelper::map($categories,'category_id','category_name');
-         return $category;
- 
-    }   
-    
-    public static function viewcategoryname($id)
-	{
-		$categories=Category::find()
-		->where(['category_allow_sale' => 'yes'])
-		->andwhere(['category_id' => $id])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->one();
-		return $categories['category_name'];
-	}
-	
-	// frontend function 
-	public static function category_slug($id)
-	{
-		$categories=Category::find()
-		->where(['category_allow_sale' => 'yes'])
-		->andwhere(['category_id' => $id])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->one();
-		return $categories;
-	}
-		// frontend function 
-	public static function category_value($slug)
-	{
-		$categoryid=Category::find()
-		->where(['slug' => $slug])
-		->andwhere(['!=', 'trash', 'Deleted'])
-		->one();
-		return $categoryid;
-	}
-
-    public static function adsbottomupdate($id)
-    {
-        foreach ($id as $key ) {
-             $c_id[] = '"'.$key.'"';               
-        }
-        $c_id = implode($c_id,',');
-        $categories = User::Category()
-		->select(['category_name'])
-		->where(['IN', 'category_id', $c_id])
-		->all();
-        return $categories;
-    }
-
-    public function upload()
-    {
-        if ($this->validate()) {
-            $this->category_icon->saveAs('uploads/' . $this->category_icon->baseName . '.' . $this->category_icon->extension);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    public static function category_search_details($name)
-	{
-			        $categories = User::Category()
-					->select(['category_id','category_name'])
-					->where(['trash' =>'Default'])
-					->andwhere(['category_allow_sale' => 'Yes'])
-					->andwhere(['like', 'category_name', $name])
-					->all();
-	}
-
-	public static function Vendorcategorylist($ids)
-	{	
-		$c = explode(",", $ids);
-		$ids = implode("','", $c);
-		$val = "'".$ids."'";
-				$categories = User::Category()
-					->select(['category_id','category_name','slug'])
-					->where(['trash' =>'Default'])
-					->andwhere(['category_allow_sale' => 'Yes'])
-					->andwhere(['category_level' => '0'])
-					->andwhere(['IN', 'category_id', $val])
-					->all();
-			return $categories; 
-	}    
+   	{			
+   		if($sale == 'yes')		
+   		return \yii\helpers\Url::to('@web/uploads/app_img/active.png');
+   		return \yii\helpers\Url::to('@web/uploads/app_img/inactive.png');
+   	}
+   	
+   	public static function statusTitle($sale)
+   	{			
+   		if($sale == 'yes')		
+   		return 'Active';
+   		return 'Deactive';
+   	}
 }
