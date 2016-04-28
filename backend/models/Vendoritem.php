96a91c2cce5add1ea6d7bbdfe4cc88f5891f37cd
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-28 18:30:21 +0530

    Added vendoritem model in backend

---
+++ backend/models/Vendoritem.php
@@ -0,0 +1,34 @@
+<?php
+namespace backend\models;
+use backend\models\Vendor;
+use Yii;
+
+class Vendoritem extends \common\models\Vendoritem
+{
+
+   public static function vendoritemmonthcount()
+    {
+        $month=date('m');
+        $year=date('Y');
+        $id=Vendor::getVendor('vendor_id');
+        return  Vendoritem::find()
+        ->where(['MONTH(created_datetime)' => $month])
+        ->andwhere(['vendor_id' => $id])
+        ->andwhere(['YEAR(created_datetime)' => $year])
+        ->count();
+    }
+
+   public static function vendoritemdatecount()
+    {
+        $date=date('d');
+        $month=date('m');
+        $year=date('Y');
+        $id=Vendor::getVendor('vendor_id');
+        return  Vendoritem::find()
+        ->where(['MONTH(created_datetime)' => $month])
+        ->andwhere(['YEAR(created_datetime)' => $year])
+        ->andwhere(['vendor_id' => $id])
+        ->andwhere(['DAYOFMONTH(created_datetime)' => $date])
+        ->count();
+    }
+}
