c326ff9fac5b4008d398f50bceb5f1a4138f408b
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-29 15:00:01 +0530

    Modified namespace in  blockdate model

--- backend/models/BlockeddateSearch.php
+++ backend/models/BlockeddateSearch.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace common\models;
+namespace backend\models;
 
 use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use common\models\Blockeddate;
+use backend\models\Blockeddate;
 
 /**
  * BlockeddateSearch represents the model behind the search form about `common\models\Blockeddate`.
