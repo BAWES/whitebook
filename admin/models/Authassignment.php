5fdf194bafe36f5105bc46a0d00517d9a04f56da
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-29 17:57:31 +0530

    Admin, authitem, auth assignment and usercontroller model added in  admin panel

---
+++ admin/models/Authassignment.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%auth_assignment}}".
+ *
+ * @property string $item_name
+ * @property string $user_id
+ * @property string $created_datetime
+ * @property string $modified_datetime
+ *
+ * @property AuthItem $itemName
+ */
+class Authassignment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%auth_assignment}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['item_name', 'user_id'], 'required'],
+           // [['created_datetime', 'modified_datetime'], 'safe'],
+            [['item_name', 'user_id'], 'string', 'max' => 64]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'item_name' => 'Item Name',
+            'user_id' => 'User ID',
+            'created_datetime' => 'Created Datetime',
+            'modified_datetime' => 'Modified Datetime',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getItemName()
+    {
+        return $this->hasOne(AuthItem::className(), ['name' => 'item_name']);
+    }
+}
