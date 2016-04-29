00ef23aabf9a952cbb253e37333d994ef86b38c9
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-29 14:58:16 +0530

    Blocked date model files added in backend

---
+++ backend/models/BlockeddateSearch.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Blockeddate;
+
+/**
+ * BlockeddateSearch represents the model behind the search form about `common\models\Blockeddate`.
+ */
+class BlockeddateSearch extends Blockeddate
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['block_id', 'vendor_id', 'created_by', 'modified_by'], 'integer'],
+            [[ 'created_datetime', 'modified_datetime', 'trash'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params,$vendor_id=false)
+    {
+        if(empty($vendor_id))
+        {          
+            $vendor_id = Vendor::getVendor('vendor_id');
+            $pagination = 40;
+        }
+        $query = Blockeddate::find()
+        ->where(['!=', 'trash', 'Deleted'])   
+        ->andwhere(['created_by'=> $vendor_id]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+			'sort'=> ['defaultOrder' => ['block_id'=>SORT_DESC]]            
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'block_id' => $this->block_id,
+            'vendor_id' => $this->vendor_id,
+            'block_date' => $this->block_date,
+            'created_by' => $this->created_by,
+            'modified_by' => $this->modified_by,
+            'created_datetime' => $this->created_datetime,
+            'modified_datetime' => $this->modified_datetime,
+        ]);
+
+        $query->andFilterWhere(['like', 'trash', $this->trash]);
+
+        return $dataProvider;
+    }
+}
