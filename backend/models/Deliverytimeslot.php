8c5eab7999fc9e145dd74fdc73a4702ba20d034c
mariyappan88 <mariyappan@designbox.com.kw>
2016-04-29 13:24:57 +0530

    Added delivery time slot model in backend model

---
+++ backend/models/Deliverytimeslot.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%vendor_delivery_timeslot}}".
+ *
+ * @property string $timeslot_id
+ * @property string $vendor_id
+ * @property string $timeslot_day
+ * @property string $timeslot_start_time
+ * @property string $timeslot_end_time
+ * @property integer $timeslot_maximum_orders
+ * @property integer $created_by
+ * @property integer $modified_by
+ * @property string $created_datetime
+ * @property string $modified_datetime
+ * @property string $trash
+ */
+class Deliverytimeslot extends \common\models\Deliverytimeslot
+{
+    public static function deliverytimeslot($day)
+    {
+        return $time_slot = Deliverytimeslot::find()->where(['vendor_id'=>Yii::$app->user->getId(), 'timeslot_day'=>$day])->asArray()->all();;
+    }
+
+
+    public static function vendor_deliverytimeslot($id,$day)
+    {
+        return $time_slot = Deliverytimeslot::find()->where(['vendor_id'=>$id, 'timeslot_day'=>$day])->asArray()->all();;
+    }
+    
+}
+
