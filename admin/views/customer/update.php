6d8036974a1bf0a09b2d10f0450694d9f9417828
whitebook-siva <Siva@designbox.com.kw>
2016-04-30 13:21:43 +0530

    customer update file changes

--- admin/views/customer/update.php
+++ admin/views/customer/update.php
@@ -10,7 +10,7 @@ $this->params['breadcrumbs'][] = ['label' => 'Customers', 'url' => ['index']];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="customer-update">
-    <?= $this->render('_form', [
+   <?= $this->render('_form', [
         'model' => $model,'model1' => $model1,'addresstype' => $addresstype,'country' => $country,'location'=>$location,'city'=>$city,
     ]) ?>
 
