e44c3b68806b3578b99b790a26594aedcfb7b787
mariyappan88 <mariyappan@designbox.com.kw>
2016-05-01 11:42:07 +0530

    Removed customer_org_password from whitebook_customer table

---
+++ console/migrations/m160501_060550_whitebook_customer.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160501_060550_whitebook_customer extends Migration
+{
+    public function up()
+    {
+        $this->dropColumn('whitebook_customer', 'customer_org_password');
+    }
+
+    public function down()
+    {
+        echo "m160501_060550_whitebook_customer cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
