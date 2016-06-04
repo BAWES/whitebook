437ecaeea6965d4092a36e1adeaeab0aba77c84e
Khalid <khalid@bawes.net>
2016-06-04 21:05:40 +0300

    Add Blameable and Timestamp behaviors to EventItemLink

--- frontend/models/Eventitemlink.php
+++ frontend/models/Eventitemlink.php
@@ -3,33 +3,36 @@
 namespace frontend\models;
 
 use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
 
 /**
- * This is the model class for table "{{%wishlist}}".
- *
- * @property integer $invitees_id
- * @property integer $event_id
- * @property string $name
- * @property string $email
- * @property string $phone_number
- * @property string $created_datetime
- * @property string $modified_datetime
- * @property integer $created_by
- * @property integer $modified_by
- */
+* This is the model class for table "{{%wishlist}}".
+*
+* @property integer $invitees_id
+* @property integer $event_id
+* @property string $name
+* @property string $email
+* @property string $phone_number
+* @property string $created_datetime
+* @property string $modified_datetime
+* @property integer $created_by
+* @property integer $modified_by
+*/
 class Eventitemlink extends \yii\db\ActiveRecord
 {
     /**
-     * @inheritdoc
-     */
+    * @inheritdoc
+    */
     public static function tableName()
     {
         return '{{%event_item_link}}';
     }
 
     /**
-     * @inheritdoc
-     */
+    * @inheritdoc
+    */
     public function rules()
     {
         return [
@@ -37,9 +40,26 @@ class Eventitemlink extends \yii\db\ActiveRecord
         ];
     }
 
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => BlameableBehavior::className(),
+                'createdByAttribute' => 'modified_by',
+                'updatedByAttribute' => 'modified_by',
+            ],
+            [
+                'class' => TimestampBehavior::className(),
+                'createdAtAttribute' => 'created_datetime',
+                'updatedAtAttribute' => 'modified_datetime',
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
     /**
-     * @inheritdoc
-     */
+    * @inheritdoc
+    */
     public function attributeLabels()
     {
         return [
