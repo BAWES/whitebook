de091a1a90190655b904d173260b6a925cad894d
Khalid Al-Mutawa <BAWES@Khalids-MacBook.zain>
2016-06-10 03:47:20 +0300

    Update migrations to work with latest version of MySQL

--- console/migrations/m160403_150838_fix_auth_issue.php
+++ console/migrations/m160403_150838_fix_auth_issue.php
@@ -7,16 +7,18 @@ class m160403_150838_fix_auth_issue extends Migration
     public function up()
     {
         //Fix columns in `whitebook_auth_item` with issues
-        $this->alterColumn("whitebook_auth_item", "created_datetime", $this->integer());
-        $this->renameColumn("whitebook_auth_item", "created_datetime", "created_at");
-        $this->alterColumn("whitebook_auth_item", "modified_datetime", $this->integer());
-        $this->renameColumn("whitebook_auth_item", "modified_datetime", "updated_at");
+        $this->dropColumn("whitebook_auth_item", "created_datetime");
+        $this->addColumn('whitebook_auth_item', 'created_at', $this->integer());
+
+        $this->dropColumn("whitebook_auth_item", "modified_datetime");
+        $this->addColumn('whitebook_auth_item', 'updated_at', $this->integer());
 
         //Fix columns in `whitebook_auth_assignment` with issues
-        $this->alterColumn("whitebook_auth_assignment", "created_datetime", $this->integer());
-        $this->renameColumn("whitebook_auth_assignment", "created_datetime", "created_at");
-        $this->alterColumn("whitebook_auth_assignment", "modified_datetime", $this->integer());
-        $this->renameColumn("whitebook_auth_assignment", "modified_datetime", "updated_at");
+        $this->dropColumn("whitebook_auth_assignment", "created_datetime");
+        $this->addColumn('whitebook_auth_assignment', 'created_at', $this->integer());
+
+        $this->dropColumn("whitebook_auth_assignment", "modified_datetime");
+        $this->addColumn('whitebook_auth_assignment', 'updated_at', $this->integer());
     }
 
     public function down()
--- console/migrations/m160410_193438_fix_slide_table.php
+++ console/migrations/m160410_193438_fix_slide_table.php
@@ -6,6 +6,7 @@ class m160410_193438_fix_slide_table extends Migration
 {
     public function up()
     {
+        $this->execute("UPDATE whitebook_slide SET modified_datetime=NOW()");
         $this->alterColumn("whitebook_slide", "slide_image", $this->string());
         $this->alterColumn("whitebook_slide", "slide_video_url", $this->string());
     }
