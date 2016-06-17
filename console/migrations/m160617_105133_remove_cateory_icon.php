97ab68314c612545ee82e28dd6ecd053635d2df5
mariyappan88 <mariyappan@designbox.com.kw>
2016-06-17 16:28:46 +0530

    Removed first level category icon field

---
+++ console/migrations/m160617_105133_remove_cateory_icon.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160617_105133_remove_cateory_icon extends Migration
+{
+    public function up()
+    {
+        $this->dropColumn('whitebook_category','category_icon');
+    }
+
+    public function down()
+    {
+        echo "m160617_105133_remove_cateory_icon cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
