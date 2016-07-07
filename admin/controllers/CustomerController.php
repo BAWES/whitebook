893cb7796198d10a1a6907a61993e3b1c9c5da49
mariyappan88 <mariyappan@designbox.com.kw>
2016-07-07 19:10:11 +0530

    Removed extra gender column in excel view on admin customer export .

--- admin/controllers/CustomerController.php
+++ admin/controllers/CustomerController.php
@@ -262,7 +262,6 @@ class CustomerController extends Controller
                 'customer_gender',
                 'customer_mobile',
                 'customer_dateofbirth',
-                'customer_gender',
               ],
         ]);
     }
