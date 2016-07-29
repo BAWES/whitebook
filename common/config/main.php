513baf743d5738fb17eab1df188ebd8553018b66
Krushn Kathrecha <kathrechakrushn@gmail.com>
2016-07-29 12:36:01 +0530

    force new css copy

--- common/config/main.php
+++ common/config/main.php
@@ -53,7 +53,7 @@ return [
         'assetManager' => [
             //Link assets -> create symbolic links to assets
             'linkAssets' => false,
-
+            'forceCopy' => true
             //append time stamps to assets for cache busting
             //'appendTimestamp' => true,
         ],
