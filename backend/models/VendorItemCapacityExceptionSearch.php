af99f2abf023904ff6eeeb9318f7c0e7c3c25f83
anilkumar <anilkumar.dhiman1@gmail.com>
2016-08-16 19:15:59 +0530

    added new correct naming convention model and controller files for vendor item capacity exception section

---
+++ backend/models/VendorItemCapacityExceptionSearch.php
@@ -0,0 +1,79 @@
+<?php
+namespace backend\models;
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+/**
+ * VendorItemCapacityExceptionSearch represents the model behind the search form about `common\models\VendorItemCapacityException`.
+ */
+class VendorItemCapacityExceptionSearch extends \common\models\VendorItemCapacityException
+{
+    public $item_name;
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['exception_id', 'exception_capacity', 'created_by', 'modified_by'], 'integer'],
+            [['exception_date', 'item_name','created_datetime', 'modified_datetime', 'trash'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params,$vendor_id=false)
+    {
+        if(empty($vendor_id))
+        {
+            $vendor_id = Vendor::getVendor('vendor_id');
+            $pagination = 40;
+        }
+        $query = Vendoritemcapacityexception::find()
+            ->orderBy(['exception_id' => SORT_DESC]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $query->leftJoin('{{%vendor_item}}','FIND_IN_SET({{%vendor_item}}.item_id,{{%vendor_item_capacity_exception}}.item_id)');
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+        if($this->exception_date!=''){
+            $this->exception_date = strtotime($this->exception_date);
+        }
+        $query->andFilterWhere([
+            'exception_id' => $this->exception_id,
+            'exception_date' => $this->exception_date,
+            'exception_capacity' => $this->exception_capacity,
+            'created_by' => $this->created_by,
+            'modified_by' => $this->modified_by,
+            'created_datetime' => $this->created_datetime,
+            'modified_datetime' => $this->modified_datetime,
+        ]);
+
+        $query->andFilterWhere(['like', '{{%vendor_item}}.item_name',$this->item_name]);
+        return $dataProvider;
+    }
+
+}
