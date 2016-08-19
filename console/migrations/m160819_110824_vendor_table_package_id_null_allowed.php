3d3618300b196414db5bcf572eaf229f95007548
anilkumar <anilkumar.dhiman1@gmail.com>
2016-08-19 16:44:36 +0530

    reverted $model->package_id = 0; and created new migration to allowed field accept null values

---
+++ console/migrations/m160819_110824_vendor_table_package_id_null_allowed.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Migration;
+
+class m160819_110824_vendor_table_package_id_null_allowed extends Migration
+{
+    public function up()
+    {
+        Yii::$app->db->createCommand('ALTER TABLE `whitebook_vendor` CHANGE `package_id` `package_id` INT(11) UNSIGNED NULL')->execute(); #package_id can be null
+    }
+
+    public function down()
+    {
+        echo "m160819_110824_vendor_table_package_id_null_allowed cannot be reverted.\n";
+
+        return false;
+    }
+}
