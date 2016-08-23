3db7025572b6f174591703bf7d027c1456cddc27
Khalid <khalid@bawes.net>
2016-08-23 16:48:32 +0300

    Remove max-length param for FAQ questions and answers

--- admin/views/faq/_form.php
+++ admin/views/faq/_form.php
@@ -18,20 +18,20 @@ use yii\widgets\ActiveForm;
     </div>
 
     <div class="form-group">
-        <?= $form->field($model, 'question',['template' => "{label}<div class='controls'>{input}</div>{hint}{error}"])->textarea(['rows' => 6,'maxlength' => 128])?>
-    </div> 
-    
-    <div class="form-group">
-        <?= $form->field($model, 'question_ar',['template' => "{label}<div class='controls'>{input}</div>{hint}{error}"])->textarea(['rows' => 6,'maxlength' => 128])?>
-    </div> 
-    	 
-    <div class="form-group">
-    	<?= $form->field($model, 'answer',['template' => "{label}<div class='controls'>{input}</div>{hint}{error}"])->textarea(['rows' => 6,'maxlength' => 128])?>
-    </div> 
+        <?= $form->field($model, 'question',['template' => "{label}<div class='controls'>{input}</div>{hint}{error}"])->textarea(['rows' => 6])?>
+    </div>
 
     <div class="form-group">
-        <?= $form->field($model, 'answer_ar',['template' => "{label}<div class='controls'>{input}</div>{hint}{error}"])->textarea(['rows' => 6,'maxlength' => 128])?>
-    </div> 
+        <?= $form->field($model, 'question_ar',['template' => "{label}<div class='controls'>{input}</div>{hint}{error}"])->textarea(['rows' => 6])?>
+    </div>
+
+    <div class="form-group">
+    	<?= $form->field($model, 'answer',['template' => "{label}<div class='controls'>{input}</div>{hint}{error}"])->textarea(['rows' => 6])?>
+    </div>
+
+    <div class="form-group">
+        <?= $form->field($model, 'answer_ar',['template' => "{label}<div class='controls'>{input}</div>{hint}{error}"])->textarea(['rows' => 6])?>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
