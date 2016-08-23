3509677029c99956c42e336c4ea181f6b6fbe5ac
Krushn Kathrecha <kathrechakrushn@gmail.com>
2016-08-23 12:43:19 +0530

    arabic field datatype from varchar to text

---
+++ console/migrations/m160823_065224_arabic_field_data_type_to_text.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+class m160823_065224_arabic_field_data_type_to_text extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        //vendor profile 
+        $this->alterColumn("whitebook_vendor", "vendor_return_policy_ar", $this->text());
+        $this->alterColumn("whitebook_vendor", "vendor_contact_address_ar", $this->text());
+        $this->alterColumn("whitebook_vendor", "vendor_contact_address", $this->text());
+        $this->alterColumn("whitebook_vendor", "short_description", $this->text());
+        $this->alterColumn("whitebook_vendor", "short_description_ar", $this->text());
+
+        //faqs 
+        $this->alterColumn("whitebook_faq", "question_ar", $this->text());
+        $this->alterColumn("whitebook_faq", "answer_ar", $this->text());
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
