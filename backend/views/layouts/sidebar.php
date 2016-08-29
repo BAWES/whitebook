c61424f8e5ec5b01cd139f4325398d0f89f66096
Khalid <khalid@bawes.net>
2016-08-29 15:01:34 +0300

    Initial work on upgrading sidebar

--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -14,21 +14,42 @@
 				<div class="username"> <span class="semi-bold">  </span></div>
 			</div>
 		</div>
-
 		<?php
-		$action = $this->context->action->id;
-		$controller = get_class($this->context);
-		$action = $this->context->action->id;
-		$menu = explode('\\',$controller);
-		$menu_act = $menu[2];
 
-		$first_menu = array('VendoritemController', 'VendoritemcapacityexceptionController');
-        $deliveryMenu = array('VendorlocationController','DeliverytimeslotController', 'BlockeddateController');
-
+		/* BEGIN Check vendor have any one item. !imporatnt for menus */
+		$checkone = Vendoritem::find()->where(['vendor_id'=>Yii::$app->user->getId(),'item_approved'=>'Yes','item_for_sale'=>'Yes'])->count();
+		/* END Check vendor have any one item. !imporatntfor menus */
 		?>
 		<!-- END MINI-PROFILE -->
 		<!-- BEGIN SIDEBAR MENU -->
 		<p class="menu-title">NAVIGATION</p>
+
+			  <?php
+			  $menuItems = [
+				  ['label' => '<i class="icon-custom-home"></i><span class="title">Dashboard</span>', 'url' => ['/site/index']],
+				  ['label' => '<i class="fa fa-certificate"></i><span class="title">Manage Item</span>', 'url' => ['/vendoritem/index']],
+				  ['label' => '<i class="fa fa-arrows"></i><span class="title">Manage Area</span>', 'url' => ['/vendorlocation/edit']],
+
+			  ];
+			  if($checkone > 0) {
+				  $menuItems[] = ['label' => '<i class="fa fa-clock-o"></i><span class="title">Delivery Time slot</span>', 'url' => ['/deliverytimeslot/index']];
+				  $menuItems[] = ['label' => '<i class="fa fa-calendar-o"></i><span class="title">Exception Dates</span>', 'url' => ['/vendor-item-capacity-exception/index']];
+				  $menuItems[] = ['label' => '<i class="fa fa-anchor"></i><span class="title">Weekly Off</span>', 'url' => ['/blockeddate/createweek']];
+				  $menuItems[] = ['label' => '<i class="fa fa-film"></i><span class="title">Block Date</span>', 'url' => ['/blockeddate/index']];
+			  }
+
+			  $menuItems[] = ['label' => '<i class="fa fa-book"></i><span class="title">My Package</span>', 'url' => ['/vendorpackages/index']];
+			  $menuItems[] = ['label' => '<i class="fa fa-university"></i><span class="title">My Profile</span>', 'url' => ['/site/profile']];
+			  echo Nav::widget([
+				  'options' => ['class' => 'left-side-bar'],
+				  'activateParents'=>true,
+				  'encodeLabels' => false,
+				  'items' => $menuItems,
+			  ]);
+
+			  ?>
+			<div class="clearfix"></div>
+
 		<ul>
 			<li class="<?php if ($menu_act == 'SiteController'  && $action == 'index') {echo "active"; } else  {echo "noactive";}?>">
 				<?= Html::a('<i class="icon-custom-home"></i><span class="title">Dashboard</span>', ['site/index'], ['class'=>'link-title']) ?>
