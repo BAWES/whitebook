219353c6007c299293aefb4f31999372151ec741
Krushn Kathrecha <kathrechakrushn@gmail.com>
2016-09-08 15:56:29 +0530

    report

--- admin/views/report/commission.php
+++ admin/views/report/commission.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\LinkPager;
 use yii\widgets\ActiveForm;
 
-$this->title = 'Package Report';
+$this->title = 'Commission Report';
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
