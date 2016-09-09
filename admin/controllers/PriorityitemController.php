e79efd44268f9469d688bcf11dce0be69f086e6d
Krushn Kathrecha <kathrechakrushn@gmail.com>
2016-09-09 13:57:54 +0530

    admin priority page - item dropdown

--- admin/controllers/PriorityitemController.php
+++ admin/controllers/PriorityitemController.php
@@ -118,31 +118,49 @@ class PriorityitemController extends Controller
     public function actionCreate()
     {
         $access = Authitem::AuthitemCheck('1', '19');
+        
         if (yii::$app->user->can($access)) {
+
             $model = new Priorityitem();
             $category = Category::loadcategoryname();
             $subcategory = Subcategory::loadsubcategoryname();
             $childcategory = ChildCategory::loadchild();
+            
             $priority = Vendoritem::find()->select(['item_id','item_name'])
-            ->where(['item_status' => 'Active'])
-            ->andwhere(['item_for_sale' => 'yes'])
-            ->andwhere(['!=', 'trash', 'Deleted'])
-			      ->all();
-            $priorityitem = ArrayHelper::map($priority, 'item_id', 'item_name');
-            if ($model->load(Yii::$app->request->post()) && ($model->validate())) {
-				    $model->priority_start_date = Yii::$app->formatter->asDate($model->priority_start_date, 'php:Y-m-d');
-      			$model->priority_end_date = Yii::$app->formatter->asDate($model->priority_end_date, 'php:Y-m-d');
-                $model->save();
-                echo Yii::$app->session->setFlash('success', 'Priority item added successfully!');
+              ->where(['item_status' => 'Active'])
+              ->andwhere(['!=', 'trash', 'Deleted'])
+  			      ->all();
 
-                return $this->redirect(['index']);
+            $priorityitem = ArrayHelper::map($priority, 'item_id', 'item_name');
+            
+            if ($model->load(Yii::$app->request->post()) && ($model->validate())) {
+  				    
+              $model->priority_start_date = Yii::$app->formatter->asDate(
+                $model->priority_start_date, 'php:Y-m-d');
+        			
+              $model->priority_end_date = Yii::$app->formatter->asDate(
+                $model->priority_end_date, 'php:Y-m-d');
+                
+              $model->save();
+              
+              Yii::$app->session->setFlash('success', 'Priority item added successfully!');
+
+              return $this->redirect(['index']);
+
             } else {
-                return $this->render('create', [
-                'model' => $model, 'priorityitem' => $priorityitem, 'category' => $category, 'subcategory' => $subcategory, 'childcategory' => $childcategory,
-            ]);
+              
+              return $this->render('create', [
+                'model' => $model, 
+                'priorityitem' => $priorityitem, 
+                'category' => $category, 
+                'subcategory' => $subcategory, 
+                'childcategory' => $childcategory,
+              ]);
             }
+
         } else {
-            echo Yii::$app->session->setFlash('danger', 'Your are not allowed to access the page!');
+            
+            Yii::$app->session->setFlash('danger', 'Your are not allowed to access the page!');
 
             return $this->redirect(['site/index']);
         }
