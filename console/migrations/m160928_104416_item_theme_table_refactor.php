f952115b6368db79ac71c794657069f4f22b9924
anilkumar <anilkumar.dhiman1@gmail.com>
2016-09-28 16:31:45 +0530

    migration for theme table refactoring

---
+++ console/migrations/m160928_104416_item_theme_table_refactor.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+class m160928_104416_item_theme_table_refactor extends Migration
+{
+    public function up()
+    {
+        $this->dropColumn('whitebook_vendor_item_theme', 'vendor_id');
+        $this->dropColumn('whitebook_vendor_item_theme', 'category_id');
+        $this->dropColumn('whitebook_vendor_item_theme', 'subcategory_id');
+        $this->dropColumn('whitebook_vendor_item_theme', 'theme_start_date');
+        $this->dropColumn('whitebook_vendor_item_theme', 'theme_end_date');
+        $this->dropColumn('whitebook_vendor_item_theme', 'created_by');
+        $this->dropColumn('whitebook_vendor_item_theme', 'modified_by');
+        $this->alterColumn('whitebook_vendor_item_theme', 'item_id', $this->integer(11));
+        $this->alterColumn('whitebook_vendor_item_theme', 'theme_id', $this->integer(11));
+    }
+
+    public function down()
+    {
+        $this->addColumn('whitebook_vendor_item_theme', 'vendor_id',$this->integer(11));
+        $this->addColumn('whitebook_vendor_item_theme', 'category_id',$this->integer(11));
+        $this->addColumn('whitebook_vendor_item_theme', 'subcategory_id',$this->integer(11));
+        $this->addColumn('whitebook_vendor_item_theme', 'theme_start_date',$this->date());
+        $this->addColumn('whitebook_vendor_item_theme', 'theme_end_date',$this->date());
+        $this->addColumn('whitebook_vendor_item_theme', 'created_by',$this->integer(11));
+        $this->addColumn('whitebook_vendor_item_theme', 'modified_by',$this->integer(11));
+        return false;
+    }
+}
