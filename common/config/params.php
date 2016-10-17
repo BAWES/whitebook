20983db08b011a320c56d2c011c40e248ee8e1f8
Krushn Kathrecha <kathrechakrushn@gmail.com>
2016-10-17 12:55:37 +0530

    admin email

--- common/config/params.php
+++ common/config/params.php
@@ -1,14 +1,12 @@
 <?php
 return [
     'user.passwordResetTokenExpire' => 3600,
-    'adminEmail' => 'khalid@thewhitebook.com.kw',
-    //testing purpose
+    'adminEmail' => 'hello@thewhitebook.com.kw',
     'supportEmail' => 'hello@thewhitebook.com.kw',
     'dateFormat'=> 'php:d/m/Y',
     'Currency' => 'KD',
     'CURRENCY_CODE' => '0',
     'SITE_NAME' => 'The White Book',
     'META_DESCRIPTION' => 'The White Book Event Planning Platform',
-    'META_KEYWORD' => 'The White Book Event Planning Platform',
-
+    'META_KEYWORD' => 'The White Book Event Planning Platform'
 ];
