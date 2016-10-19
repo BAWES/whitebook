eb99537b317b557197c1369097bcf9f38714c01c
Krushn Kathrecha <kathrechakrushn@gmail.com>
2016-10-19 14:36:36 +0530

    tap parameter

--- common/config/params.php
+++ common/config/params.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
     'user.passwordResetTokenExpire' => 3600,
     'adminEmail' => 'hello@thewhitebook.com.kw',
@@ -8,5 +9,13 @@ return [
     'CURRENCY_CODE' => '0',
     'SITE_NAME' => 'The White Book',
     'META_DESCRIPTION' => 'The White Book Event Planning Platform',
-    'META_KEYWORD' => 'The White Book Event Planning Platform'
+    'META_KEYWORD' => 'The White Book Event Planning Platform',
+    'tap_merchantid' => '195516',
+    'tap_username' => 'Jj2z4@tap',
+    'tap_password' => 'Jj2z4@q8',
+    'tap_api_key' => '55tap19',
+    'tap_test_merchantid' => '1230',
+    'tap_test_username' => 'test',
+    'tap_test_password' => '123tap',
+    'tap_test_api_key' => '1tap7'
 ];
