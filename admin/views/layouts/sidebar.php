4247a66a0c558866ec3bcc280b0abed409d1096e
anilkumar <anilkumar.dhiman1@gmail.com>
2016-10-31 15:01:18 +0530

    extended menu item

--- admin/views/layouts/sidebar.php
+++ admin/views/layouts/sidebar.php
@@ -24,45 +24,6 @@ $item_pending_count = VendorItem::item_pending_count();
 	<p class="menu-title">NAVIGATION</p>
 
 	<?php
-
-	$acc_report_comm = AccessControlList::can('report', 'commission');
-	$acc_report_package = AccessControlList::can('report', 'package');
-
-	$acc_siteinfo = AccessControlList::can('site-info', 'index');
-	$acc_contact = AccessControlList::can('contacts', 'index');
-	$acc_social = AccessControlList::can('social-info', 'index');
-	$acc_slide = AccessControlList::can('slide', 'index');
-	$acc_admin = AccessControlList::can('admin', 'index');
-	$acc_role = AccessControlList::can('role', 'index');
-	$acc_faqgroup = AccessControlList::can('faq-group', 'index');
-	$acc_faq = AccessControlList::can('faq', 'index');
-	$acc_adverthome = AccessControlList::can('advert-home', 'index');
-	$acc_order_status = AccessControlList::can('order-status', 'index');
-	$acc_pg = AccessControlList::can('payment-gateway', 'index');
-
-	$acc_country = AccessControlList::can('country', 'index');
-	$acc_governorate = AccessControlList::can('city', 'index');
-	$acc_location = AccessControlList::can('location', 'index');
-	$acc_addresstype = AccessControlList::can('address-type', 'index');
-	$acc_addressquestion = AccessControlList::can('address-question', 'index');
-
-	$acc_category = AccessControlList::can('category', 'index');
-	$acc_manage_subcategory = AccessControlList::can('category', 'manage_subcategory'); 
-	$acc_child_category_index = AccessControlList::can('category', 'child_category_index');
-
-	$acc_package =  AccessControlList::can('package', 'index');
-	$acc_vendor = 	AccessControlList::can('vendor', 'index');
-	$acc_vendoritem = AccessControlList::can('vendor-item', 'index');
-
-	$acc_featuregroup = AccessControlList::can('feature-group', 'index');
-	$acc_themes = AccessControlList::can('themes', 'index');
-	$acc_itemtype = AccessControlList::can('item-type', 'index');
-	$acc_priorityitem = AccessControlList::can('priority-item', 'index');
-	$acc_eventtype = AccessControlList::can('event-type', 'index');
-
-	$acc_customer = AccessControlList::can('customer', 'index');
-	$acc_events = AccessControlList::can('events', 'index');
-
 	echo \common\widgets\MenuExtended::widget([
 	    'items' => [
 	        [	
@@ -81,8 +42,6 @@ $item_pending_count = VendorItem::item_pending_count();
 	        	'label' => '<i class="fa fa-university"></i><span class="title">General Settings</span>',
 				'options' => ['class'=>'dropdown'],
 				'template' => '<a href="javascript:;">{label}<span class="arrow"></span></a>',
-				'visible' => ($acc_siteinfo || $acc_contact || $acc_social || $acc_slide || $acc_admin || $acc_role ||
-					$acc_faqgroup || $acc_faq || $acc_adverthome || $acc_order_status || $acc_pg) ? true : false,
 				'items' => [
 					['label' => 'Site Info', 'url' => ['site-info/index']],
 					['label' => 'Contact Enquiries', 'url' => ['contacts/index']],
@@ -101,7 +60,6 @@ $item_pending_count = VendorItem::item_pending_count();
 	        	'label' => '<i class="fa fa-anchor"></i><span class="title">Manage Address</span>',
 				'options'=>['class'=>'dropdown'],
 				'template' => '<a href="javascript:;">{label}<span class="arrow"></span></a>',
-				'visible' => ($acc_country || $acc_governorate || $acc_location || $acc_addresstype || $acc_addressquestion) ? true : false,
 				'items' => [
 					['label' => 'Manage Country', 'url' => ['country/index']],
 					['label' => 'Manage Governorate', 'url' => ['city/index']],
@@ -114,8 +72,7 @@ $item_pending_count = VendorItem::item_pending_count();
 	        	'label' => '<i class="fa fa-bell"></i><span class="title">Manage Category</span>',
 				'options'=> ['class'=>'dropdown'],
 				'template' => '<a href="javascript:;">{label}<span class="arrow"></span></a>',
-				'visible' => ($acc_category || $acc_manage_subcategory || $acc_child_category_index) ? true : false,
-				'items' => [					
+				'items' => [
 					['label' => 'Level I', 'url' => ['category/index']],
 					['label' => 'Level II', 'url' => ['category/manage_subcategory']],
 					['label' => 'Level III', 'url' => ['category/child_category_index']],
@@ -125,7 +82,6 @@ $item_pending_count = VendorItem::item_pending_count();
 	        	'label' => '<i class="fa fa-arrows"></i><span class="title">Manage Vendor</span>',
 				'options'=>['class'=>'dropdown'],
 				'template' => '<a href="javascript:;">{label}<span class="arrow"></span></a>',
-				'visible' => ($acc_package || $acc_vendor || $acc_vendoritem) ? true : false,
 				'items' => [
 					['label' => 'Manage Package', 'url' => ['package/index']],
 					['label' => 'Manage Vendor', 'url' => ['vendor/index']],
@@ -136,8 +92,7 @@ $item_pending_count = VendorItem::item_pending_count();
 	        	'label' => '<i class="fa fa-certificate"></i><span class="title">Manage Item</span>',
 				'options'=>['class'=>'dropdown'],
 				'template' => '<a href="javascript:;">{label}<span class="arrow"></span></a>',
-				'visible' => ($acc_featuregroup || $acc_themes || $acc_itemtype || $acc_priorityitem || $acc_eventtype) ? true:false, 
-				'items' => [				
+				'items' => [
 					['label' => 'Manage Group', 'url' => ['feature-group/index']],
 					['label' => 'Manage Themes Days', 'url' => ['themes/index']],
 					['label' => 'Manage Item Type', 'url' => ['item-type/index']],
@@ -149,7 +104,6 @@ $item_pending_count = VendorItem::item_pending_count();
 	        	'label' => '<i class="glyphicon glyphicon-user"></i><span class="title">Manage Customer</span>',
 				'options'=>['class'=>'dropdown'],
 				'template' => '<a href="javascript:;">{label}<span class="arrow"></span></a>',
-				'visible' => ($acc_customer || $acc_events)? true: false,
 				'items' => [
 					['label' => 'Customer', 'url' => ['customer/index']],
 					['label' => 'Customer Events', 'url' => ['events/index']],
@@ -159,24 +113,14 @@ $item_pending_count = VendorItem::item_pending_count();
 	        	'label' => '<i class="fa fa-bullseye"></i><span class="title">Reports</span>',
 				'options'=>['class'=>'dropdown'],
 				'template' => '<a href="javascript:;">{label}<span class="arrow"></span></a>',
-				'visible' => ($acc_report_package || $acc_report_comm) ? true : false,
 				'items' => [
-					[
-						'label' => 'Vendor packages', 
-						'url' => ['report/package'],
-						'visible' => $acc_report_package
-					],
-					[
-						'label' => 'Vendor commission', 
-						'url' => ['report/commission'],
-						'visible' => $acc_report_comm
-					]
+					['label' => 'Vendor packages', 'url' => ['report/package']],
+					['label' => 'Vendor commission', 'url' => ['report/commission']]
 				]
 			],
 			[	
 	        	'label' => '<i class="fa fa-tasks"></i><span class="title">Static Pages</span>', 
-	        	'url' => ['cms/index'],
-	        	'visible' => AccessControlList::can('cms/index')
+	        	'url' => ['cms/index']
 	        ],
 	    ],
 	    'encodeLabels' => false,
