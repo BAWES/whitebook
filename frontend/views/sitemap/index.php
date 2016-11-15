1efa1357e0c5aca60788dbbfb4447c34de27eac7
anilkumar <anilkumar.dhiman1@gmail.com>
2016-11-15 15:34:39 +0530

    site map issue resolved

--- frontend/views/sitemap/index.php
+++ frontend/views/sitemap/index.php
@@ -1,10 +1,11 @@
 <?php 
 
 use yii\helpers\Url;
-
+header ("Content-Type:text/xml");
 ?>
-<xml version="1.0" encoding="UTF-8">
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">
   <!-- categories -->
   <?php foreach($categories as $category) { ?>
   <url>
