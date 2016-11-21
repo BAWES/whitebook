38303e8dec412ce4ccd5bc279f3f2bbb9b64b390
Krushn Kathrecha <kathrechakrushn@gmail.com>
2016-11-21 17:08:37 +0530

    Revert "category + package was not working in create vendor form"
    This reverts commit 6a2e3d2fe12cd756646bbd8bbabd3113c38957fa.

--- admin/models/Role.php
+++ admin/models/Role.php
@@ -127,7 +127,7 @@ class Role extends \yii\db\ActiveRecord
             'slide' => ['index', 'view', 'create', 'update', 'delete', 'Sort_slide', 'status', 'block'],
             'social-info' => ['index', 'view', 'create', 'update', 'delete'],
             'themes' => ['index', 'view', 'create', 'update', 'delete', 'block'],
-            'vendor' => ['index', 'view', 'vendoritemview', 'password', 'create', 'update', 'delete', 'loadcategory', 'loadsubcategory', 'emailcheck', 'block', 'changepackage', 'changeeditpackage', 'loadpackagedate', 'packageupdate', 'vendornamecheck', 'validate-vendor'],
+            'vendor' => ['index', 'view', 'vendoritemview', 'password', 'create', 'update', 'delete', 'loadcategory', 'loadsubcategory', 'emailcheck', 'block', 'changepackage', 'changeeditpackage', 'loadpackagedate', 'packageupdate', 'vendornamecheck', 'validateVendor'],
             'vendor-item' => ['index', 'view', 'create', 'update', 'delete', 'check', 'block', 'approve', 'status', 'removequestion', 'sort_vendor_item', 'addquestion', 'guideimage', 'renderquestion', 'viewrenderquestion', 'renderanswer', 'galleryupload', 'ttemgallery', 'salesguideimage', 'deletesalesimage', 'deleteitemimage', 'deleteserviceguideimage', 'itemnamecheck', 'upload-cropped-image'],
             'vendor-item-pending' => ['index'],
             'vendor-draft-item' => ['index', 'approve'],
