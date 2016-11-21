f7a309d6a3dfbbd81b5a823ffe62b45684e692a1
Khalid <khalid@bawes.net>
2016-11-21 13:22:09 +0300

    Fix migration bug

--- console/migrations/m161028_072258_super_admin_permission.php
+++ console/migrations/m161028_072258_super_admin_permission.php
@@ -7,7 +7,6 @@ class m161028_072258_super_admin_permission extends Migration
 {
     public function up()
     {
-        $this->dropColumn('whitebook_siteinfo', 'super_admin_role_id');
         $this->addColumn('whitebook_siteinfo', 'super_admin_role_id', $this->integer(11)->defaultValue(1)->after('commision'));
     }
 }
