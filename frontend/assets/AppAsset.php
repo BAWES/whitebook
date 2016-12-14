c1f235885fd024b4729c5b69b819f987577a5589
Krushn Kathrecha <kathrechakrushn@gmail.com>
2016-12-14 12:27:00 +0530

    package listing page

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,10 +18,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/style.css?v=1.17',
+        'css/style.css?v=1.18',
         'fonts/flaticon/flaticon.css',
         'css/demo.css?v=1.2',
-        'css/media_style.css?v=1.6',
+        'css/media_style.css?v=1.7',
         'css/owl.carousel.css',
         'css/ma5-mobile-menu.css',
         'css/bootstrap-select.min.css',
