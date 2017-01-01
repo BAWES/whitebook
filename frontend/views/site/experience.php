2008094156f3bfad187ea05b7c3f89bbc7322135
Khalid <khalid@bawes.net>
2017-01-01 15:02:10 +0300

    Hotfix to hide features that arent ready for prod

--- frontend/views/site/experience.php
+++ frontend/views/site/experience.php
@@ -41,8 +41,9 @@ use yii\helpers\Html;
         </div>
 
         <div class="add_banner">
-            <?= Html::img("@web/images/explore_banner.jpg") ?>
+            <?php /* echo Html::img("@web/images/explore_banner.jpg"); */ ?>
         </div>
+
 
     </div>
 </section>
