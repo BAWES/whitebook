e356aae3d88c8efbe1cb22e0b2f0226248c50b1f
Khalid <khalid@bawes.net>
2017-01-09 13:15:22 +0300

    Setup API alias

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,6 +4,7 @@ Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('admin', dirname(dirname(__DIR__)) . '/admin');
+Yii::setAlias('api', dirname(dirname(__DIR__)) . '/api');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('temp_folder', dirname(__DIR__) . '/temp');
 
