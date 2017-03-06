31d98ea7bc859826132f620719b9c39f3ce6fbdd
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-03-06 14:02:41 +0530

    admin commission report

--- admin/controllers/ReportController.php
+++ admin/controllers/ReportController.php
@@ -5,9 +5,7 @@ namespace admin\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\data\Pagination;
-use common\models\VendorPackages;
-use common\models\Suborder;
-use admin\models\Package;
+use common\models\Booking;
 use admin\models\Vendor;
 use admin\models\AccessControlList;
 
@@ -49,7 +47,7 @@ class ReportController extends Controller
             ->all();
 
         //result query  
-        $query = Suborder::getReportQuery([
+        $query = Booking::getReportQuery([
                 'date_start' => $date_start,
                 'date_end' => $date_end,
                 'group_by' => $group_by,
@@ -74,53 +72,6 @@ class ReportController extends Controller
             'pagination' => $pagination,
             'vendors' => $vendors,
             'groups' => $groups
-        ]);
-    }
-
-    public function actionPackage()
-    {
-    	$date_start = Yii::$app->request->get('date_start');
-    	$date_end = Yii::$app->request->get('date_end');
-    	$group_by = Yii::$app->request->get('group_by');
-    	$package_id = Yii::$app->request->get('package_id'); 
-    	
-    	$packages = Package::find()
-    		->where(['package_status' => 'Active', 'trash' => 'Default'])
-    		->all();
-
-    	$groups = [
-    		'day' => 'Day',
-    		'week' => 'Week',
-    		'month' => 'Month',
-    		'year' => 'Year'
-    	];
-    	
-    	//result query 	
-    	$query = VendorPackages::getReportQuery([
-    			'date_start' => $date_start,
-		    	'date_end' => $date_end,
-		    	'group_by' => $group_by,
-		    	'package_id' => $package_id
-    		]);	
-
-    	// create a pagination object with the total count
-		$pagination = new Pagination(['totalCount' => $query->count()]);
-
-		// limit the query using the pagination and retrieve the result
-		$result = $query->offset($pagination->offset)
-		    ->limit($pagination->limit)
-		    ->asArray()
-		    ->all();
-
-        return $this->render('package', [
-        	'date_start' => $date_start,
-	    	'date_end' => $date_end,
-	    	'group_by' => $group_by,
-	    	'package_id' => $package_id,
-	    	'packages' => $packages,
-	    	'result' => $result,
-	    	'pagination' => $pagination,
-	    	'groups' => $groups
         ]);
     }
 }
