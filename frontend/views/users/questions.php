1931c4b11de6b8ecd15e53e35360eb11b7e36f18
Anil Kumar <anilkumar.dhiman1@gmail.com>
2017-03-17 10:52:25 +0530

    asterisk implemented at checkout

--- frontend/views/users/questions.php
+++ frontend/views/users/questions.php
@@ -13,7 +13,7 @@
 	<div class="col-md-6">
 		<div class="form-group">
 			<label class="control-label" for="question[<?= $question['ques_id'] ?>]">
-				<?= \common\components\LangFormat::format($question['question'],$question['question_ar']); ?>
+				<?= \common\components\LangFormat::format($question['question'],$question['question_ar']); ?><?=($question['required']) ? '*' : '';?>
 			</label>
 			<div class="controls">
 				<input type="text" id="question[<?= $question['ques_id'] ?>]" class="<?= $class ?>" name="question[<?= $question['ques_id'] ?>]" value="<?= isset($question['response_text'])?$question['response_text']:'' ?>" />
