a164cbfa3effa54c6575d05a9ee303369ebe3e86
Krushn Kathrecha <kathrechakrushn@gmail.com>
2017-05-22 14:30:41 +0530

    delivery date time info in checkout

--- frontend/views/checkout/address.php
+++ frontend/views/checkout/address.php
@@ -9,8 +9,9 @@ use common\models\Booking;
 
 $customer_address_modal->area_id = Yii::$app->session->get('delivery-location');
 
-?>
+require '_delivery.php';
 
+?>
 <div class="panel panel-default">
         <div class="panel-heading">
                 <h4 class="panel-title">
